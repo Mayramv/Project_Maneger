09af258d52ed1fcbff9edf599ba8b70511edb7b0
Mayra Malaquias <mayra.malaquias@hotmail.com>
2023-02-23 14:53:54 -0300

    primeiro commit

---
+++ src/main/java/model/Project.java
@@ -0,0 +1,69 @@
+package model;
+
+import java.util.Date;
+
+
+public class Project {
+   private int id;
+   private String name;
+   private String description;
+   private Date createdAt;
+   private Date UpdatedAt;
+
+    public Project(int id, String name, String description, Date createdAt, Date UpdatedAt) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.createdAt = createdAt;
+        this.UpdatedAt = UpdatedAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return UpdatedAt;
+    }
+
+    public void setUpdatedAt(Date UpdatedAt) {
+        this.UpdatedAt = UpdatedAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Project{" + "id=" + id + ", name=" + name + ", description=" + description + ", createdAt=" + createdAt + ", UpdatedAt=" + UpdatedAt + '}';
+    }
+   
+   
+   
+}
+
